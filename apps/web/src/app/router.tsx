a1aa639c59e1367bed668ff379ed1c56df144da7
INRI CHAIN <contact@inri.life>
2026-03-25 16:10:28 -0300

    Update router.tsx

--- apps/web/src/app/router.tsx
+++ apps/web/src/app/router.tsx
@@ -8,13 +8,18 @@ import { MyNFTsPage } from '../pages/MyNFTsPage'
 import { ExplorePage } from '../pages/ExplorePage'
 import { NotFoundPage } from '../pages/NotFoundPage'
 
-export const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/create-collection', element: <CreateCollectionPage /> },
-  { path: '/my-collections', element: <MyCollectionsPage /> },
-  { path: '/collections/:address', element: <CollectionDetailsPage /> },
-  { path: '/collections/:address/create-nft', element: <CreateNFTPage /> },
-  { path: '/my-nfts', element: <MyNFTsPage /> },
-  { path: '/explore', element: <ExplorePage /> },
-  { path: '*', element: <NotFoundPage /> },
-])
+export const router = createBrowserRouter(
+  [
+    { path: '/', element: <HomePage /> },
+    { path: '/create-collection', element: <CreateCollectionPage /> },
+    { path: '/my-collections', element: <MyCollectionsPage /> },
+    { path: '/collections/:address', element: <CollectionDetailsPage /> },
+    { path: '/collections/:address/create-nft', element: <CreateNFTPage /> },
+    { path: '/my-nfts', element: <MyNFTsPage /> },
+    { path: '/explore', element: <ExplorePage /> },
+    { path: '*', element: <NotFoundPage /> },
+  ],
+  {
+    basename: '/inri-studio/',
+  }
+)
